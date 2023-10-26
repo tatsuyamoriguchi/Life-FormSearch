ff6c1d68bab4a983eb4de2f878f250b568276a70
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2023-10-26 12:42:36 -0700

    func prettyPrintedJSONString()

---
+++ Life-FormSearch/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Life-FormSearch
+//
+//  Created by Tatsuya Moriguchi on 10/26/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    let scrollView = UIScrollView
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension Data {
+    func prettyPrintedJSONString() {
+        guard let jsonObject = try? JSONSerialization.jsonObject(with: self, options: []), let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted]), let prettryJSONString = String(data: jsonData, encoding: .utf8) else {
+            print("Failed to read JSON obejct for prettyPrintedJSONString()")
+        }
+        print(prettryJSONString)
+                
+    }
+}
